e51753a28b35782f40e5fd4c32e6d8d22614663f
rahulkrishnan221 <rahulkrishnan221@gmail.com>
2018-02-04 16:07:33 +0530

    Image slider added

--- app/src/main/java/com/priwil/ejoytv/home_fragment.java
+++ app/src/main/java/com/priwil/ejoytv/home_fragment.java
@@ -1,21 +1,124 @@
 package com.priwil.ejoytv;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by SURYA on 2/1/2018.
  */
 
+
+
+
 public class home_fragment extends Fragment {
+    private ViewPager vp_slider;
+    private LinearLayout ll_dots;
+    SliderPagerAdapter sliderPagerAdapter;
+    ArrayList<String> slider_image_list;
+    private TextView[] dots;
+    int page_position = 0;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.activity_home, container, false);
+
+// method for initialisation
+        init(view);
+
+// method for adding indicators
+        addBottomDots(0);
+
+        final Handler handler = new Handler();
+
+        final Runnable update = new Runnable() {
+            public void run() {
+                if (page_position == slider_image_list.size()) {
+                    page_position = 0;
+                } else {
+                    page_position = page_position + 1;
+                }
+                vp_slider.setCurrentItem(page_position, true);
+            }
+        };
+
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, 100, 5000);
     return view;
+    }
+
+    private void init(View view) {
+
+
+        vp_slider = (ViewPager) view.findViewById(R.id.vp_slider);
+        ll_dots = (LinearLayout) view.findViewById(R.id.ll_dots);
+
+        slider_image_list = new ArrayList<>();
+
+//Add few items to slider_image_list ,this should contain url of images which should be displayed in slider
+// here i am adding few sample image links, you can add your own
+
+        slider_image_list.add("http://images.all-free-download.com/images/graphiclarge/mountain_bongo_animal_mammal_220289.jpg");
+        slider_image_list.add("http://images.all-free-download.com/images/graphiclarge/bird_mountain_bird_animal_226401.jpg");
+        slider_image_list.add("http://images.all-free-download.com/images/graphiclarge/mountain_bongo_animal_mammal_220289.jpg");
+        slider_image_list.add("http://images.all-free-download.com/images/graphiclarge/bird_mountain_bird_animal_226401.jpg");
+
+
+        sliderPagerAdapter = new SliderPagerAdapter(getActivity(), slider_image_list);//check;
+        vp_slider.setAdapter(sliderPagerAdapter);
+
+        vp_slider.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                addBottomDots(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[slider_image_list.size()];
+
+        ll_dots.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(getActivity());//check
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(Color.parseColor("#000000"));
+            ll_dots.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(Color.parseColor("#FFFFFF"));
     }
 }
